7e46617df8037f91348823e15f869d97046c83aa
Matthew Torres <matthew.torres211@gmail.com>
2020-12-13 18:03:58 +0800

    Add small animation

--- src/components/Explorer.js
+++ src/components/Explorer.js
@@ -1,14 +1,37 @@
 import React, { useEffect, useState } from 'react'
 import { Box } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom'
+import { motion } from 'framer-motion'
 import { fetch } from '../fetch';
 import { getEmployeeData, searchEmployees } from '../helpers';
 import EmployeeProfile from './EmployeeProfile';
 
+const Container = motion.custom(Box)
 // For caching - need to refactor for stale data
 let employeeCopy = {}
 
 const Explorer = () => {
+  const list = {
+    visible: {
+      opacity: 1,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.3,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      transition: {
+        when: "afterChildren",
+      },
+    },
+  }
+  
+  const item = {
+    visible: { opacity: 1, x: 0 },
+    hidden: { opacity: 0, x: -100 },
+  }
+
   const { name } = useParams()
   const [employees, setEmployees] = useState({})
   const [isFetching, setIsFetching] = useState(false)
@@ -20,10 +43,10 @@ const Explorer = () => {
     }
 
     return juniors.map((junior, idx) => (
-      <Box key={`${junior}-idx`} ml={level * 4}>
+      <Container key={`${junior}-idx`} ml={level * 4} variants={item}>
         <EmployeeProfile employee={{ name: junior, ...employees[junior] }} />
         {renderSubordinates(employees[junior].subordinates, newLevel)}
-      </Box>
+      </Container>
     ))
   }
 
@@ -57,14 +80,14 @@ const Explorer = () => {
   }
 
   return (
-    <Box px='6' mx='auto' width='min(70ch, 100%)'>
+    <Container px='6' mx='auto' width='min(70ch, 100%)' initial="hidden" animate="visible" variants={list}>
       {employees[name] &&
         <>
           <EmployeeProfile employee={{ name, ...employees[name] }} />
           {renderSubordinates(employees[name].subordinates)}
         </>
       }
-    </Box>
+    </Container>
   )
 }
 
